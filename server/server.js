87cc030c3da36cc74ce552f20878e0e6777a3251
Silva <daniel.felipe.silva@accenture.com>
2025-09-29 12:42:30 -0300

    pushing dependencies

--- server/server.js
+++ server/server.js
@@ -152,4 +152,4 @@ app.use((err, _req, res, _next) => {
 
 // --- Listen ---
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
